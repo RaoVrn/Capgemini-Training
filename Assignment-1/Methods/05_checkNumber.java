2825942594ae71cf01cb6ba1937e1c5a59508a58
RaoVrn <prakash.varun.0305@gmail.com>
2025-12-12 11:21:55 +0530

    Add NumberCheck program to determine if a number is positive, negative, or zero

---
+++ Assignment-1/Methods/05_checkNumber.java
@@ -0,0 +1,33 @@
+/*
+5. Write a program to check whether a number is positive, negative, or zero.
+Hint => Get integer input from the user. Write a Method to return -1 for negative number, 1 for
+positive number and 0 if number is zero
+*/
+
+import java.util.*;
+
+public class NumberCheck {
+
+    // Method returns -1, 0, or 1
+    public static int checkNumber(int number) {
+
+        if (number < 0)
+            return -1;
+        else if (number > 0)
+            return 1;
+        else
+            return 0;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        int result = checkNumber(number);
+
+        System.out.println("Result: " + result);
+    }
+}
